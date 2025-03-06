4a357f25868cb52719e51bf9514b1293eb3c885b
Vusi Mathonsi <mathonsi@duck.com>
2025-03-06 20:09:21 +0200

    Implement navigation click handling and update icon paths for improved navigation state management

--- assets/js/handlers/clickHandlers.js
+++ assets/js/handlers/clickHandlers.js
@@ -1,5 +1,53 @@
+
 export function handleModalCloseClick() {
     document.querySelector('.modal').remove();
     document.querySelector('.modal-overlay').remove();
     document.body.style.overflow = 'visible';
+}
+
+export function navigationClick() {
+    document.querySelector('nav').removeEventListener('click', navigationClickHandler);
+    document.querySelector('nav').addEventListener('click', navigationClickHandler);
+}
+
+export function navigationClickHandler(e){
+    const clickedElement = e.target.closest('.nav-item');
+
+    const page = clickedElement.querySelector('span').innerHTML.toLowerCase().trim();
+
+    if(clickedElement) controlNavigationItemsState(page)
+}
+
+export function controlNavigationItemsState(page = 'home'){
+    const iconPathName = './assets/media/icons';
+
+    const navItems = document.querySelectorAll('.nav-item');
+    const navIcons = document.querySelectorAll('.nav-icon');
+
+    const icons = ['home', 'classes', 'students', 'summary'];
+
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        navIcons.forEach((icon, i)=>{
+            icon.src = `${iconPathName}/${icons[i]}-secondary-400.png`;
+
+            navItems[i].classList.remove('active')
+            // Style the active page nav item
+            if(icons[i] === page) {
+                icon.src = `${iconPathName}/${icons[i]}-primary-400-active.png`;
+                navItems[i].classList.add('active');
+            }
+        });
+    } else {
+        navIcons.forEach((icon, i)=>{
+            icon.src = `${iconPathName}/${icons[i]}-secondary-500.png`;
+
+            navItems[i].classList.remove('active')
+            // Style the active page nav item
+            if(icons[i] === page) {
+                icon.src = `${iconPathName}/${icons[i]}-primary-500-active.png`;
+                navItems[i].classList.add('active');
+            }
+        });
+
+    }
 }
