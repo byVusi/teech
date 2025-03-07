c639307e7110722b8c977b6591f9d47570c3abbe
Vusi Mathonsi <mathonsi@duck.com>
2025-03-07 19:37:10 +0200

    Updated main.js, service-worker.js, Alerts.js and Modals.js

--- assets/js/builders/components/Modals.js
+++ assets/js/builders/components/Modals.js
@@ -3,6 +3,14 @@ import {createButton} from './Buttons.js';
 import { handleModalCloseClick } from "../../handlers/clickHandlers.js";
 import { Validator } from "../../classes/Validator.js";
 
+/**
+ * Creates a modal element.
+ * @param {string|HTMLElement} [header='Modal header'] - The modal header, can be a string or an HTML element.
+ * @param {string|HTMLElement} [body=''] - The modal body, can be a string or an HTML element.
+ * @param {string|HTMLElement} [footer=''] - The modal footer, can be a string or an HTML element.
+ * @param {Object} [options={}] - Additional options for modal customization.
+ * @returns {HTMLElement} The generated modal element.
+ */
 export function createModal(header = 'Modal header', body = '', footer = '', options = {}) {
     const modal = createNewElement('div', { attributes: { class: 'modal', ...options.attributes } });
 
@@ -17,6 +25,12 @@ export function createModal(header = 'Modal header', body = '', footer = '', opt
     return modal;
 }
 
+/**
+ * Creates a modal header element.
+ * @param {string} [title='Modal title'] - The title of the modal header.
+ * @param {Object} [options={}] - Additional options for modal header customization.
+ * @returns {HTMLElement} The generated modal header element.
+ */
 export function createModalHeader( title = 'Modal title', options = {} ) {
     if(!Validator.isNonEmptyString(title)) {
         console.error('Invalid title. Title must be non-empty string. Defaulting to "Modal title"...');
@@ -29,13 +43,17 @@ export function createModalHeader( title = 'Modal title', options = {} ) {
     }
  
     const modalHeader = createNewElement('div', {text: title, attributes: { class: 'modal-header', ...options.attributes } });
-
     const closeButton = createButton('', { attributes: { class: 'btn close-button' }, events: { 'click': handleModalCloseClick } }, { name: 'close', size: 'large'} );
-
     modalHeader.append(closeButton);
     return modalHeader;
 }
 
+/**
+ * Creates a modal body element.
+ * @param {string} [content=''] - The content of the modal body.
+ * @param {Object} [options={}] - Additional options for modal body customization.
+ * @returns {HTMLElement} The generated modal body element.
+ */
 export function createModalBody( content = '', options = {} ) {
     if(content && !Validator.isString(content)) {
         console.error('Invalid content. Content must be a valid HTML element.');
@@ -48,11 +66,16 @@ export function createModalBody( content = '', options = {} ) {
     }
 
     const modalBody = createNewElement('div', { attributes: { class: 'modal-body', ...options.attributes} });
-
     modalBody.append(content);
     return modalBody;
 }
 
+/**
+ * Creates a modal footer element.
+ * @param {HTMLElement[]} [content=['Modal footer']] - An array of valid HTML elements for the footer.
+ * @param {Object} [options={}] - Additional options for modal footer customization.
+ * @returns {HTMLElement} The generated modal footer element.
+ */
 export function createModalFooter(content = ['Modal footer'], options = {}) {
     if(!Validator.isValidHTMLElement(...content)) {
         console.error('Invalid content. Content must be a valid HTML element.');
@@ -65,11 +88,14 @@ export function createModalFooter(content = ['Modal footer'], options = {}) {
     }
 
     const modalFooter = createNewElement('div', { attributes: { class: 'modal-footer', ...options.attributes } });
-
     modalFooter.append(...content);
     return modalFooter;
 }
 
+/**
+ * Creates a modal overlay element.
+ * @returns {HTMLElement} The generated modal overlay element.
+ */
 function createModalOverlay() {
     const overlay = createNewElement('div', { attributes: { class: 'modal-overlay' } });
     return overlay;
