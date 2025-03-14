ddca77b7ff11ec6119d9e926c9a5b7016e308879
Vusi Mathonsi <mathonsi@duck.com>
2025-03-14 09:10:53 +0200

    Removed formatting.js, refactored dom.js and filter.js

--- assets/js/utils/dom.js
+++ assets/js/utils/dom.js
@@ -1,15 +1,10 @@
 import { Validator } from "../classes/Validator.js";
 
-export function returnElementFromDOM(elementSelector) {
-	if (!isElementInDOM(elementSelector)) {
-		console.error("Cannot return a non-existant element.");
-		return;
-	}
-
-	const element = document.querySelector(elementSelector);
-	return element;
-}
-
+/**
+ * Checks if an element exists in the DOM.
+ * @param {string} elementSelector - The selector of the element to check.
+ * @returns {boolean} - True if the element exists, false otherwise.
+ */
 export function isElementInDOM(elementSelector) {
 	if (!Validator.isValidString(elementSelector)) {
 		console.error(
@@ -26,6 +21,27 @@ export function isElementInDOM(elementSelector) {
 	return true;
 }
 
+/**
+ * Returns an element from the DOM.
+ * @param {string} elementSelector - The selector of the element to return.
+ * @returns {Element|null} - The DOM element if found, otherwise null.
+ */
+export function returnElementFromDOM(elementSelector) {
+	if (!isElementInDOM(elementSelector)) {
+		console.error("Cannot return a non-existant element.");
+		return;
+	}
+
+	const element = document.querySelector(elementSelector);
+	return element;
+}
+
+/**
+ * Inserts a child element into a parent element in the DOM.
+ * @param {string} parentElementSelector - The selector of the parent element.
+ * @param {string} childElementSelector - The selector of the child element.
+ * @param {Function} buildFunction - The function to build the child element.
+ */
 export function insertContainerInDOM(
 	parentElementSelector,
 	childElementSelector,
--- assets/js/utils/filter.js
+++ assets/js/utils/filter.js
@@ -1,24 +1,47 @@
 import { Validator } from "../classes/Validator";
 
-export function filterByClass(arr, className) {
+/**
+ * Sorts an array of objects by a specified criteria.
+ *
+ * @param {Object[]} arr - The array to sort.
+ * @param {string} [criteria="lastName"] - The criteria to sort by.
+ * @returns {Object[]} The sorted array.
+ */
+export function sortArray(arr = [], criteria = "lastName") {
+	return arr.sort((a, b) =>
+		(a[criteria] || "").localeCompare(b[criteria] || "", undefined, {
+			sensitivity: "base",
+		})
+	);
+}
+
+/**
+ * Filters an array of objects based on a specified criteria and sorts the result by a specified criteria.
+ *
+ * @param {Object[]} arr - The array to filter.
+ * @param {string} [filterCriteria="className"] - The criteria to filter by.
+ * @param {string} [sortCriteria="lastName"] - The criteria to sort by.
+ * @returns {Object[]} The filtered and sorted array.
+ * @throws {Error} If the first argument is not a valid array.
+ */
+export function filterArray(
+	arr,
+	filterCriteria = "className",
+	sortCriteria = "lastName"
+) {
 	if (!Validator.isValidArray(arr)) {
 		throw new Error("Expected an array as the first argument.");
 	}
 
-	const validatedClassName = Validator.validateString(className);
+	const validatedFilterCriteria = Validator.validateString(filterCriteria);
+	const validatedSortCriteria = Validator.validateString(sortCriteria);
 
-	return sortByLastName(
+	return sortArray(
 		arr.filter(
 			(student) =>
-				student?.className && student?.className === validatedClassName
-		)
-	);
-}
-
-export function sortByLastName(arr = []) {
-	return arr.sort((a, b) =>
-		(a.lastName || "").localeCompare(b.lastName || "", undefined, {
-			sensitivity: "base",
-		})
+				student[filterCriteria] &&
+				student[filterCriteria] === validatedFilterCriteria
+		),
+		validatedSortCriteria
 	);
 }
